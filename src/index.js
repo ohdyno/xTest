9316f6daa5423feaf1c34b4f9c7a85e48441e4fc
xing <xing@integral.io>
2020-08-14 09:39:55 -0400

    Scope expect into xTest namespace

--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
-function expect(actual) {
-  let negated = false;
-  return {
-    toEqual(expected) {
-      let test;
-      let message;
-      if (negated) {
-        message = `Expected '${actual}' to not equal '${expected}'`;
-        test = () => actual !== expected;
-      } else {
-        message = `Expected '${actual}' to equal '${expected}'`;
-        test = () => actual === expected;
-      }
-      console.assert(test(), message);
-      return {
-        actual,
-        expected,
-        message,
-      };
-    },
-    get not() {
-      negated = true;
-      return this;
-    },
-  };
-}
+const xTest = {
+  expect(actual) {
+    let negated = false;
+    return {
+      toEqual(expected) {
+        let test;
+        let message;
+        if (negated) {
+          message = `Expected '${actual}' to not equal '${expected}'`;
+          test = () => actual !== expected;
+        } else {
+          message = `Expected '${actual}' to equal '${expected}'`;
+          test = () => actual === expected;
+        }
+        console.assert(test(), message);
+        return {
+          actual,
+          expected,
+          message,
+        };
+      },
+      get not() {
+        negated = true;
+        return this;
+      },
+    };
+  },
+};
 
-expect(true).toEqual(true);
-expect(true).not.toEqual(false);
+xTest.expect(true).toEqual(true);
+xTest.expect(true).not.toEqual(false);
 
-let result = expect(true).toEqual(true);
-expect(result.message).toEqual(`Expected '${true}' to equal '${true}'`);
-expect(result.actual).toEqual(true);
-expect(result.expected).toEqual(true);
+let result = xTest.expect(true).toEqual(true);
+xTest.expect(result.message).toEqual(`Expected '${true}' to equal '${true}'`);
+xTest.expect(result.actual).toEqual(true);
+xTest.expect(result.expected).toEqual(true);
