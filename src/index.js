91bf1afc4d1f32ced3a7258b03656185a725392e
xing <xing@integral.io>
2020-08-14 00:43:06 -0400

    Improve the assertion failure message for equality check

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ function expect(actual) {
   let negated = false;
   return {
     toEqual(expected) {
-      let result = actual === expected;
-      if (negated) {
-        result = !result;
-      }
-      console.assert(result, `Expected: ${expected}. Actual: ${actual}`);
+        let result = actual === expected;
+        if (negated) {
+            console.assert(!result, `Expected '${actual}' to not equal '${expected}'`);
+        } else {
+            console.assert(result, `Expected '${actual}' to equal '${expected}'`);
+        }
     },
-    get not() {
-      negated = true;
-      return this;
-    }
-  }
+      get not() {
+          negated = true;
+          return this;
+      },
+  };
 }
 
 expect(true).toEqual(true);
