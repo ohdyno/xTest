80dc487201cc21dbd0c016890dbcf65a683b19e5
xing <xing@integral.io>
2020-08-14 01:05:14 -0400

    Return result object from a single assertion

--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@ function expect(actual) {
   let negated = false;
   return {
     toEqual(expected) {
-      let result = actual === expected;
+      let test;
+      let message;
       if (negated) {
-        console.assert(
-          !result,
-          `Expected '${actual}' to not equal '${expected}'`,
-        );
+        message = `Expected '${actual}' to not equal '${expected}'`;
+        test = () => actual !== expected;
       } else {
-        console.assert(result, `Expected '${actual}' to equal '${expected}'`);
+        message = `Expected '${actual}' to equal '${expected}'`;
+        test = () => actual === expected;
       }
+      console.assert(test(), message);
+      return {
+        actual,
+        expected,
+        message,
+      };
     },
     get not() {
       negated = true;
@@ -21,3 +27,8 @@ function expect(actual) {
 
 expect(true).toEqual(true);
 expect(true).not.toEqual(false);
+
+let result = expect(true).toEqual(true);
+expect(result.message).toEqual(`Expected '${true}' to equal '${true}'`);
+expect(result.actual).toEqual(true);
+expect(result.expected).toEqual(true);
