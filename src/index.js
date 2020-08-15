c83f542e85b24a86778b36d4898c5c40a0b7b0bc
xing <xing@integral.io>
2020-08-15 08:27:05 -0400

    Extract expect into stand-alone function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import xTest from './xTest';
 
-xTest.test('test case', (t) => {
-  let testCase = xTest.test('test name', (t) => {
+xTest.test('positive test case', (t) => {
+  let testCase = xTest.test('positive test case', (t) => {
     t.expect(true).toEqual(true);
   });
 
-  t.expect(testCase.name).toEqual('test name');
+  t.expect(testCase.name).toEqual('positive test case');
   t.expect(testCase.result).toEqual({
     actual: true,
     expected: true,
-    message: `Expected '${true}' to equal '${true}'`,
+    message: `Expected 'true' to equal 'true'`,
+  });
+});
+
+xTest.test('negative test case', (t) => {
+  let testCase = xTest.test('negative test case', (t) => {
+    t.expect(true).not.toEqual(false);
+  });
+
+  t.expect(testCase.name).toEqual('negative test case');
+  t.expect(testCase.result).toEqual({
+    actual: true,
+    expected: false,
+    message: `Expected 'true' to not equal 'false'`,
   });
 });
--- src/xTest.js
+++ src/xTest.js
@@ -1,41 +1,49 @@
 import _ from 'lodash-es';
 
+let expect = (resultRecorder) => (actual) => {
+  let negated = false;
+  return {
+    toEqual(expected) {
+      let test;
+      let message;
+      if (negated) {
+        message = `Expected '${JSON.stringify(
+          actual,
+        )}' to not equal '${JSON.stringify(expected)}'`;
+        test = () => !_.isEqual(actual, expected);
+      } else {
+        message = `Expected '${JSON.stringify(
+          actual,
+        )}' to equal '${JSON.stringify(expected)}'`;
+        test = () => _.isEqual(actual, expected);
+      }
+      console.assert(test(), message);
+      let result = {
+        actual,
+        expected,
+        message,
+      };
+      resultRecorder.record(result);
+      return result;
+    },
+    get not() {
+      negated = true;
+      return this;
+    },
+  };
+};
+
 const xTest = {
   test(name, t) {
     let testCase = {};
-    testCase.expect = (actual) => {
-      let negated = false;
-      return {
-        toEqual(expected) {
-          let test;
-          let message;
-          if (negated) {
-            message = `Expected '${JSON.stringify(
-              actual,
-            )}' to not equal '${JSON.stringify(expected)}'`;
-            test = () => !_.isEqual(actual, expected);
-          } else {
-            message = `Expected '${JSON.stringify(
-              actual,
-            )}' to equal '${JSON.stringify(expected)}'`;
-            test = () => _.isEqual(actual, expected);
-          }
-          console.assert(test(), message);
-          let result = {
-            actual,
-            expected,
-            message,
-          };
-          testCase.result = result;
-          return result;
-        },
-        get not() {
-          negated = true;
-          return this;
-        },
-      };
+    testCase.record = (r) => {
+      testCase.result = r;
     };
+
+    testCase.expect = expect(testCase);
+
     t(testCase);
+
     return {
       name,
       result: testCase.result,
