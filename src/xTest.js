dce3459bd42369fd3d401ceea2df56c62ec42fe8
xing <xing@integral.io>
2020-08-21 22:38:31 -0400

    Throw Error when assertion fails instead of console.assert()

--- src/xTest.js
+++ src/xTest.js
@@ -1,8 +1,9 @@
 function assert(actual, expected) {
-  console.assert(
-    actual === expected,
-    `Expected ${actual} to equal ${expected}`,
-  );
+  if (actual !== expected) {
+    throw new Error(
+      `Assertion failed: Expected ${actual} to equal ${expected}`,
+    );
+  }
 }
 
 class Expect {
