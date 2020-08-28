e68f99df8bac00de110692967d99979d91c6ab23
xing <xing@integral.io>
2020-08-27 22:40:18 -0400

    Remove unnecessary TestCase class

--- src/TestCase/TestCase.js
+++ src/TestCase/TestCase.js
@@ -16,36 +16,26 @@ ${JSON.stringify(
   }
 }
 
-export class TestCase {
-  constructor(name, body) {
-    this.body = body;
-    this.result = {
-      name,
-      successes: [],
-      failures: [],
-    };
-  }
+export function test(name, body, resultHandler = new TestCaseResultHandler()) {
+  const result = {
+    name,
+    successes: [],
+    failures: [],
+  };
 
-  run(resultHandler = new TestCaseResultHandler()) {
-    const result = this.result;
-    const resultRecorder = {
-      success(message) {
-        result.successes.push(message);
-      },
-      fail(message) {
-        result.failures.push(message);
-      },
-    };
+  const resultRecorder = {
+    success(message) {
+      result.successes.push(message);
+    },
+    fail(message) {
+      result.failures.push(message);
+    },
+  };
 
-    this.body({
-      expect: (actual) => expect(actual, resultRecorder),
-      fail: (message) => fail(message, resultRecorder),
-    });
+  body({
+    expect: (actual) => expect(actual, resultRecorder),
+    fail: (message) => fail(message, resultRecorder),
+  });
 
-    resultHandler.result(result);
-  }
-}
-
-export function test(name, body, resultHandler) {
-  new TestCase(name, body).run(resultHandler);
+  resultHandler.result(result);
 }
