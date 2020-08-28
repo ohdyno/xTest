2f27e68d2d4615fcd55c2d892075038fce0d6772
xing <xing@integral.io>
2020-08-27 21:27:17 -0400

    Remove unnecessary Fail class

--- src/xTest.js
+++ src/xTest.js
@@ -9,16 +9,6 @@ export function test(name, body, resultHandler) {
   new TestCase(name, body).run(resultHandler);
 }
 
-class Fail {
-  constructor(message) {
-    this.message = message;
-  }
-
-  run(resultHandler) {
-    resultHandler.fail({ message: this.message });
-  }
-}
-
 export function fail(message, resultHandler) {
-  new Fail(message).run(resultHandler);
+  resultHandler.fail({ message });
 }
