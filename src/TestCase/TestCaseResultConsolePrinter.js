c61a3e60955d3058d16d5ff9337f6f5145ff4e6f
xing <xing@integral.io>
2020-08-30 10:09:57 -0400

    Improve console output with groups and colors

--- src/TestCase/TestCaseResultConsolePrinter.js
+++ src/TestCase/TestCaseResultConsolePrinter.js
@@ -1,35 +1,41 @@
 export class TestCaseResultConsolePrinter {
   success(result) {
-    console.log(`Test Case Passed:
-Name: ${result.name}
-Successes:
-${JSON.stringify(
-  result.successes,
-  (key, value) => (typeof value === 'undefined' ? 'undefined' : value),
-  4,
-)}
-Failures:
-${JSON.stringify(
-  result.failures,
-  (key, value) => (typeof value === 'undefined' ? 'undefined' : value),
-  4,
-)}`);
+    console.group(`%cTest Case Passed: ${result.name}`, 'color:green');
+    console.group(`Successes`);
+    result.successes.forEach((success) => {
+      console.log(`${JSON.stringify(
+        success,
+        (key, value) => (typeof value === 'undefined' ? 'undefined' : value),
+        4,
+      )}
+      `);
+    });
+    console.groupEnd();
+    console.groupEnd();
   }
 
   fail(result) {
-    console.error(`Test Case Passed:
-Name: ${result.name}
-Successes:
-${JSON.stringify(
-  result.successes,
-  (key, value) => (typeof value === 'undefined' ? 'undefined' : value),
-  4,
-)}
-Failures:
-${JSON.stringify(
-  result.failures,
-  (key, value) => (typeof value === 'undefined' ? 'undefined' : value),
-  4,
-)}`);
+    console.group(`%cTest Case Failed: ${result.name}`, 'color:red');
+    console.group(`Successes`);
+    result.successes.forEach((success) => {
+      console.log(`${JSON.stringify(
+        success,
+        (key, value) => (typeof value === 'undefined' ? 'undefined' : value),
+        4,
+      )}
+      `);
+    });
+    console.groupEnd();
+    console.group(`%cFailures`, 'color:red');
+    result.failures.forEach((failure) => {
+      console.error(`${JSON.stringify(
+        failure,
+        (key, value) => (typeof value === 'undefined' ? 'undefined' : value),
+        4,
+      )}
+      `);
+    });
+    console.groupEnd();
+    console.groupEnd();
   }
 }
